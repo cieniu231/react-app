605532d1ce2ba7f1f11d2788e4088c47e42d3464
cieniu231 <cieniu231@o2.pl>
2023-02-18 02:59:31 +0100

    first commit

---
+++ my-application/src/components/navigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+class Menu extends React.Component {
+    render(){
+       return (
+        <div>
+            <ul>
+                <li>Rotterdam</li>
+                <li>Haga</li>
+                <li>Amsterdam</li>
+                <li>Paryż</li>
+                <li>Bruksela</li>
+                <li>Antwerpia</li>
+                <li>Berlin</li>
+            </ul>
+        </div>
+        )
+    }
+}
+
+export default Menu
